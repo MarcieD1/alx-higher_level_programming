f8bfb48118d00e16ec3d91324e26648bcc88dd7e
MarcieD1 <marciatarisai@icloud.com>
2023-10-31 16:44:47 -0500

    Bytecode 2

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+def magic_calculation(a, b, c):
+    """
+    Performs calculations based on the given conditions.
+    """
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    return a * b - c
