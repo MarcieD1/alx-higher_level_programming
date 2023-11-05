d2b9d763a13f6ec139c2ee1fb91cb26302a8c2a9
MarcieD1 <marciatarisai@icloud.com>
2023-11-05 11:27:37 -0600

    New Lists Error

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
+    copy = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list.copy()
     else:
-        new_list = my_list.copy()
-        new_list[idx] = element
-        return new_list
-
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-print(new_list)
-print(my_list)
+        copy[idx] = element
+        return copy
