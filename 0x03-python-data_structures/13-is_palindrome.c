48c1892a68f944d4efd0a81df17cdc4e640bcfca
MarcieD1 <marciatarisai@icloud.com>
2023-11-06 14:19:45 -0600

    Fix 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,53 +3,33 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for project
- */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
-
-/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to the head of the list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = *head;
-    listint_t *fast = *head;
-    listint_t *prev = NULL;
-    listint_t *next = NULL;
+    listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
 
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
-
-    while (fast != NULL && fast->next != NULL)
-    {
-        fast = fast->next->next;
-        next = slow->next;
-        slow->next = prev;
-        prev = slow;
-        slow = next;
-    }
-
-    if (fast != NULL)
-        slow = slow->next;
-
-    while (prev != NULL && slow != NULL)
-    {
-        if (prev->n != slow->n)
-            return (0);
-        prev = prev->next;
-        slow = slow->next;
-    }
-
-    return (1);
+	if (*head == NULL || head2->next == NULL)
+		return (1);
+	while (head2 != NULL)
+	{
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
+	}
+	aux2 = aux;
+	while (*head != NULL)
+	{
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
+			return (0);
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
+	}
+	free_listint(aux);
+	return (1);
 }
