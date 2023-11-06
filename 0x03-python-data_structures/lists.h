2a7b86447099fe8671640d3e1ed7d6f5036a54ce
MarcieD1 <marciatarisai@icloud.com>
2023-11-06 15:31:32 -0600

    Fix It all 13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,30 +1,28 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-/* Structure for singly linked list */
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+
+ */
 typedef struct listint_s
 {
     int n;
     struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
 size_t print_listint(const listint_t *h);
-int element_at(const listint_t *head, unsigned int index);
-listint_t *replace_in_list(listint_t *head, unsigned int index, int value);
-void print_reversed_list_integer(const listint_t *head);
-listint_t *new_in_list(const listint_t *head, int index, int value);
-char *no_c(char *str);
-void print_matrix_integer(const int (*matrix)[3]);
-int *add_tuple(const int *tuple_a, const int *tuple_b);
-int multiple_returns(char *sentence, int *length, char *first);
-int max_integer(const int *array, int size);
-int *divisible_by_2(const int *array, int size);
-int *delete_at_index(int *array, unsigned int size, unsigned int index);
-void switch_integers(int *a, int *b);
-int is_palindrome(listint_t **head);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+int is_palindrome(listint_t **head);
+int aux_palind(listint_t **head, listint_t *end);
 
 #endif /* LISTS_H */
