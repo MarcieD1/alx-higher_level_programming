2ec4fd48f450161fcdf364a3a814faa585bb1257
MarcieD1 <marciatarisai@icloud.com>
2023-11-06 09:48:15 -0600

    Returns

--- 0x03-python-data_structures/8-main.py
+++ 0x03-python-data_structures/8-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from 8_multiple_returns import multiple_returns
+multiple_returns = __import__('8-multiple_returns').multiple_returns
 
 sentence = "At school, I learnt C!"
 length, first = multiple_returns(sentence)
