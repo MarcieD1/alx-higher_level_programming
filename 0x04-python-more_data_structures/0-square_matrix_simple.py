64a504a71f1a44277426e33bb4f5d4184a6fed11
MarcieD1 <marciatarisai@icloud.com>
2023-11-08 16:59:54 -0600

    Simple Squared Fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,8 @@
 
 def square_matrix_simple(matrix):
     new_matrix = []
-
     for i in range(len(matrix)):
-        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-
-    return (new_matrix)
+        new_matrix.append([])
+        for j in range(len(matrix[i])):
+            new_matrix[i].append(matrix[i][j] ** 2)
+    return new_matrix
