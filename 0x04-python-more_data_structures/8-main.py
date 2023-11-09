447536db56d1a9d69ccda96d7b2681346c8479a9
MarcieD1 <marciatarisai@icloud.com>
2023-11-08 19:03:25 -0600

    A Simple Delete

--- 0x04-python-more_data_structures/8-main.py
+++ 0x04-python-more_data_structures/8-main.py
@@ -5,12 +5,19 @@ def simple_delete(a_dictionary, key=""):
         del a_dictionary[key]
     return a_dictionary
 
+
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary.keys()):
         print("{}: {}".format(key, a_dictionary[key]))
 
+
 if __name__ == "__main__":
-    a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+    a_dictionary = {
+        'language': "C",
+        'Number': 89,
+        'track': "Low",
+        'ids': [1, 2, 3]
+    }
     new_dict = simple_delete(a_dictionary, 'track')
     print_sorted_dictionary(a_dictionary)
     print("--")
