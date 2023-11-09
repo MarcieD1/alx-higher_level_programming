a7238ef56323d0e28663f9e2c021c0aaf788d6b7
MarcieD1 <marciatarisai@icloud.com>
2023-11-08 19:38:03 -0600

    The Best Score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,6 +8,7 @@ def best_score(a_dictionary):
                 return key
     return None
 
+
 if __name__ == "__main":
     a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
     best_key = best_score(a_dictionary)
