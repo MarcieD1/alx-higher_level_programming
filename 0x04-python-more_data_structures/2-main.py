dc10ab01798d55a8d8d4f113aabbe8587531e91a
MarcieD1 <marciatarisai@icloud.com>
2023-11-08 18:02:40 -0600

    Making Addition Unique

--- 0x04-python-more_data_structures/2-main.py
+++ 0x04-python-more_data_structures/2-main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
-def uniq_add(my_list=[]):
+def unique_add(my_list=[]):
     unique_list = set(my_list)
     num = 0
-
-    for i in uniq_lists:
+    for i in unique_list:
         num += i
-
-    return sum_unique
+    return num
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 1, 4, 2, 5]
-    result = uniq_add(my_list)
+    result = unique_add(my_list)
     print("Result: {:d}".format(result))
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    unique_list = set(my_list)
-    num = 0
-
-    for i in uniq_lists:
-        num += i
-
+    unique_set = set(my_list)
+    sum_unique = sum(unique_set)
     return sum_unique
