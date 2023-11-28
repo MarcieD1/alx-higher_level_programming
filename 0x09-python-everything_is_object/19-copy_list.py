b397a8782ffca5f882de7e123edb76dabe8d6c8a
MarcieD1 <marciatarisai@icloud.com>
2023-11-28 08:56:47 -0600

    Finish Line

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-
-def copy_list(l):
+def copy_list(a):
   """Return a copy of the list."""
-  return l[:]
-
-def main():
-  """Copy a list."""
-  l = [1, 2, 3]
-  copy_l = copy_list(l)
-  print(copy_l)
-
-if __name__ == "__main__":
-  main()
+  return a.copy()
