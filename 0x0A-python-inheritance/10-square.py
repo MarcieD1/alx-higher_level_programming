0187aa5148e5cbb9eb604670248fe8a371d8ced7
MarcieD1 <marciatarisai@icloud.com>
2023-12-04 15:34:17 -0600

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""Defines a Square class that inherits from Rectangle."""
+
+
+class BaseGeometry:
+    """A class with area and integer_validator methods."""
+
+    def area(self):
+        """Raises an Exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value as an integer.
+
+        Args:
+            name (str): The name associated with the value.
+            value: The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """A class representing a rectangle."""
+
+    def __init__(self, width, height):
+        """Initializes a new rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Computes the area of the rectangle."""
+        return self.__width * self.__height
+
+
+class Square(Rectangle):
+    """A class representing a square."""
+
+    def __init__(self, size):
+        """Initializes a new square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
