16bda23d0e52c1d4d66dd27d8c1c44b81b3085ac
MarcieD1 <marciatarisai@icloud.com>
2023-12-05 09:16:26 -0600

    Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Add all arguments to a Python list and save them to a file."""
+import sys
+import json
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file.
+
+    Returns:
+        object: The Python data structure represented by the JSON file.
+    """
+    try:
+        with open(filename, mode="r", encoding="utf-8") as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
