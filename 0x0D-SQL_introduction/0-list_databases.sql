faee645c52c2ad4af1de75c9ff944928db107d5b
MarcieD1 <marciatarisai@icloud.com>
2024-01-16 09:38:57 -0600

    Listing the Databases

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,12 @@
+-- Lists all databases of a MySQL server
+-- Connect to the MySQL server
+-- Get all databases
+-- Print the databases
+
+-- Connect to the MySQL server
+-- Get all databases
+-- Print the databases
+
+-- Connect to the MySQL server
+-- Get all databases
+-- Print the databases
