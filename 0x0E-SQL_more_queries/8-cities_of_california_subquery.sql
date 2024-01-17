877804cec0f448e28df47c4a16c1c25ba3e46cf0
MarcieD1 <marciatarisai@icloud.com>
2024-01-17 16:57:07 -0600

    Fixing Cali

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,7 @@
 -- List all cities of California from hbtn_0d_usa
-SELECT cities.name
-FROM cities, states
-WHERE cities.state_id = states.id AND states.name = 'California'
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id IN (
+  SELECT id FROM states WHERE name = 'California'
+)
 ORDER BY cities.id ASC;
---
+++ 0x0E-SQL_more_queries/hbtn_0d_usa.sql
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>17QFBWKCCXENAVAW</RequestId><HostId>LCl9benAq1S8WhImMXNeSqbdgLwL/PECtiEisBrnIymK61e063nXPxQzvVC6l8ryamZbbnVg/1o=</HostId></Error>
