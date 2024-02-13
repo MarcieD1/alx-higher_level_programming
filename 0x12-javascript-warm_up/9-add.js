ed28bc70a60f610f5f58b695182dd1b0dcbe4399
MarcieD1 <marciatarisai@icloud.com>
2024-02-12 20:20:11 -0600

    Fix The addition of Integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -7,16 +7,18 @@ function add(a, b) {
 
 // Get the first and second arguments passed to the script
 // eslint-disable-next-line no-console
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
+const arg1 = parseInt(process.argv[2], 10);
+const arg2 = parseInt(process.argv[3], 10);
 
 // Check if both arguments are valid integers
 // eslint-disable-next-line no-console
-if (isNaN(arg1) || isNaN(arg2)) {
-  console.log("NaN");
+if (Number.isNaN(arg1) || Number.isNaN(arg2)) {
+  // eslint-disable-next-line no-console
+  console.log('NaN');
 } else {
   // Call the add function and print the result
   // eslint-disable-next-line no-console
   const result = add(arg1, arg2);
+  // eslint-disable-next-line no-console
   console.log(result);
 }
