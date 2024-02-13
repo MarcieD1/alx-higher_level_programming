0b8a652a8b07cf9cf1444c3f5618774bb4a73187
MarcieD1 <marciatarisai@icloud.com>
2024-02-12 19:38:24 -0600

    This script prints 3 lines in multi-languages: C, Python and Javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
 // Create an array of strings
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 // Iterate over the array and print each element using console.log
-for (let i = 0; i < languages.length; i++) {
+for (let i = 0; i < languages.length; i += 1) {
+  // eslint-disable-next-line no-console
   console.log(languages[i]);
 }
