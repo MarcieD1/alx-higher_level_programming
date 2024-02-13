0a7892c3005ad5c1a84c311ac557e7742910cb12
MarcieD1 <marciatarisai@icloud.com>
2024-02-12 19:25:17 -0600

    This script prints 3 lines in multi-languages: C, Python and Javascript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Create an array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Iterate over the array and print each element using console.log
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
