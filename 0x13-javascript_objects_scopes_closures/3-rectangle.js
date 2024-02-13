a949bd1b0a0482314ed6dda621e239e385fc75a6
MarcieD1 <marciatarisai@icloud.com>
2024-02-13 09:36:22 -0600

    Fix the indentation

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,20 +2,20 @@
 
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
+    if (w > 0 && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 
   print() {
     for (let i = 0; i < this.height; i += i) {
-      let row = '';
+      let s = '';
       for (let j = 0; j < this.width; j += j) {
-        row += 'X';
+        s += 'X';
       }
-      console.log(row);
+      // eslint-disable-next-line no-console
+      console.log(s);
     }
   }
 }
