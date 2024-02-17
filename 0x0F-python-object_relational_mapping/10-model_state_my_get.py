ad85f734f8caed88f20a4464da42b6f95eb339b2
MarcieD1 <marciatarisai@icloud.com>
2024-02-17 05:13:11 -0600

    a script that prints the State object with the name passed as argument from the database

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
+"""Module that searches for a state in a MySQL database using SQLAlchemy."""
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Create the SQLAlchemy engine using the provided MySQL credentials
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create a session factory
     Session = sessionmaker(bind=engine)
+
+    # Create a session object
     session = Session()
-    state_name = sys.argv[4]
-    state = session.query(State).filter(State.name == state_name).first()
-    if state:
-        print(state.id)
-    else:
+
+    # Search for the specified state in the database
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+
+    # Print a message if the state is not found
+    if found is False:
         print("Not found")
-    session.close()
