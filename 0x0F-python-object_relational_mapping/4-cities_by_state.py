b10975b599876942231e30bc60650b2db8168017
MarcieD1 <marciatarisai@icloud.com>
2024-02-16 22:15:39 -0600

    These are all the cities in the database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,6 +3,7 @@
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
 			passwd=sys.argv[2], db=sys.argv[3] port=3306)
@@ -12,5 +13,5 @@ if __name__ == "__main__":
     rows = cur.fetchall()
     for row in rows:
 	print(row)
-    cursor.close()
+    cur.close()
     db.close()
