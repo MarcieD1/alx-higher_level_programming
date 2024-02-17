01f250f256233e2a8c0921ed91605fadd86e11e8
MarcieD1 <marciatarisai@icloud.com>
2024-02-16 21:14:32 -0600

    This script displays an argument for all values in the states table

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,9 +4,10 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (sys.argv[4],))
-    for row in cursor.fetchall():
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY", '{}'
+                .format(sys.argv[4]))
+    for row in rows:
         print(row)
-    cursor.close()
+    cur.close()
     db.close()
