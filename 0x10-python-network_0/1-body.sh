21431f739dad3e7866bac4dc86739d1f81c1f985
MarcieD1 <marciatarisai@icloud.com>
2024-02-29 18:33:58 -0600

    This script takes a URL, sends a GET request, and displays the body of the response for a 200 status

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request, and displays the body of the response for a 200 status code
-curl -s -o /dev/null -w "%{http_code}" "$1" -X GET -L -s -f -o /tmp/body | cat /tmp/body
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
+if [ "$response" == "200" ]; then
+    curl -s "$1"
+fi
