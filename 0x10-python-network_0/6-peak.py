dfe0fd930413bcb7d4bd3052c33bafcd9fe15aa0
MarcieD1 <marciatarisai@icloud.com>
2024-02-29 18:19:25 -0600

    Module to find a peak in a list of unsorted integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,13 @@ Module to find a peak in a list of unsorted integers
 """
 
 def find_peak(list_of_integers):
+    """
+    Function to find a peak in a list of unsorted integers
+    Args:
+        list_of_integers: List of integers to search for a peak
+    Returns:
+        The peak value found in the list
+    """
     if not list_of_integers:
         return None
     peak = max(list_of_integers)
